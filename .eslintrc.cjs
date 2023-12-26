d8561cd1a9a9284d5f5da47479265053cb7b6040
Sergei Fomenko <fomenko.personal@gmail.com>
2023-12-27 02:20:46 +0800

    tooling: add eslint plugins

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -5,14 +5,29 @@ module.exports = {
     'eslint:recommended',
     'plugin:@typescript-eslint/recommended',
     'plugin:react-hooks/recommended',
+    'plugin:perfectionist/recommended-natural',
+    'plugin:prettier/recommended'
   ],
   ignorePatterns: ['dist', '.eslintrc.cjs'],
   parser: '@typescript-eslint/parser',
-  plugins: ['react-refresh'],
+  plugins: ['react-refresh', 'simple-import-sort'],
   rules: {
     'react-refresh/only-export-components': [
       'warn',
       { allowConstantExport: true },
     ],
+    'prettier/prettier': 'error',
+    'perfectionist/sort-imports': 'off',
+    'perfectionist/sort-named-imports': 'off',
+    'simple-import-sort/imports': [
+      'error',
+      {
+        groups: [
+          ['^react', '^node', '^'],
+          ['^@src', '^\\.', '^\\u0000'],
+        ],
+      },
+    ],
+    'simple-import-sort/exports': 'error'
   },
 }
